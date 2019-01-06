35427b61d9a54da1a3b6951e5700cb11f02c8707
s4s7 <per0aspera2ad0astra2@gmail.com>
2019-01-06 23:15:31 +0900

    Add urls_table to restriction

--- db/migrate/20190105123229_create_urls.rb
+++ db/migrate/20190105123229_create_urls.rb
@@ -1,10 +1,10 @@
 class CreateUrls < ActiveRecord::Migration[5.2]
   def change
     create_table :urls do |t|
-      t.string :name
-      t.string :thumbnail
-      t.integer :subscriber
-      t.integer :view
+      t.string :name, null:false
+      t.string :thumbnail, null:false
+      t.integer :subscriber, null:false
+      t.integer :view, null:false
       t.timestamps
     end
   end
