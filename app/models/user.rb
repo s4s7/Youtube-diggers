b4ca8775a2a1e71ea5ecf40c6a6ad81e100c539e
s4s7 <per0aspera2ad0astra2@gmail.com>
2019-01-07 23:17:23 +0900

    Add association to user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :urls, through: :urls_users
+  has_many :urls_users
 end
