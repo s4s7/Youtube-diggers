011512d05d7b344cd86554c92747beb73175e067
s4s7 <per0aspera2ad0astra2@gmail.com>
2019-01-21 01:31:35 +0900

    Add category_save_function to category_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,22 @@
 class CategoriesController < ApplicationController
-    def create
+  def create
+    #category
+    @category = Category.new(category_params)
+    if @category.save
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: '投稿できました' }
+        format.json
+      end
+    else
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
