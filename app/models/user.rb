37cfc59bf5dea88335a1cc00e7ae3a37b6bc1bfc
s4s7 <per0aspera2ad0astra2@gmail.com>
2019-01-21 13:06:27 +0900

    Update association of use_model to latest db_design

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :urls, through: :urls_users
-  has_many :urls_users
+  has_many :urls, through: :first_urls
+  has_many :second_urls
+  has_many :third_urls
 end
