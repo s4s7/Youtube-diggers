3bab557d4381ff14cb4c57e7587f6214fa5b2829
s4s7 <per0aspera2ad0astra2@gmail.com>
2019-01-22 23:23:53 +0900

    Add fadeIn function to url_form

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -3,6 +3,7 @@ $(document).on('turbolinks:load', function() {
     // Function-level strict mode syntax
     'use strict';
 
+    //category
     var $filters = $('.filter [filter]'), $boxes = $('.category [category]'), $input = $('input');
 
     $filters.on('click', function(e) {
@@ -20,4 +21,16 @@ $(document).on('turbolinks:load', function() {
       $('#category_name').val($value)
     });
 
+
+    //url_form
+  $('#first_url_name').change(function() {
+    $("#second_url_name").fadeIn(500);
+    $("#second_name").fadeIn(1000);
+    $('#second_name').css('display','block');
+  });
+  $('#second_url_name').change(function() {
+    $("#third_url_name").fadeIn(500);
+    $("#third_name").fadeIn(1000);
+    $('#third_name').css('display','block');
+  });
 });
