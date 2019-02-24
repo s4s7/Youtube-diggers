31a861e27a90cc19d0220f7a472c460256c0070a
s4s7 <per0aspera2ad0astra2@gmail.com>
2019-02-24 14:40:31 +0900

    Add asynchronous_communication for search_category

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -8,7 +8,6 @@ $(document).on('turbolinks:load', function() {
 
     $filters.on('click', function(e) {
       e.preventDefault();
-      // var $this = $(this);
       var $filterCategory = $(this).attr('filter');
       $boxes.fadeOut().promise().done(function() {
         $boxes.filter('[category = "' + $filterCategory + '"]').addClass('is-animated').fadeIn();
@@ -19,8 +18,52 @@ $(document).on('turbolinks:load', function() {
       e.preventDefault();
       var value = $(this).attr('value');
       $('#category_name').val(value);
-    });
 
+      // カテゴリー別ランキング
+      var ranking_result = $(".form_research");
+      function rankingUrl(data, num){
+        var html = `<div class='card'>
+                      <div class='card__image'>
+                        <a target='blank' href= '${data.url}' >
+                          <img height='180' src='${data.thumbnail}'>
+                        </a>
+                      </div>
+                      <div class='card__content'>
+                        <div class='card__content_order'>${num}</div>
+                        <div class='card__content_info'>
+                          <div class='card__content_info_title'>
+                            ${data.title}
+                          </div>
+                          <div class='card__content_info_detail'>
+                            ${data.author}
+                          </div>
+                          <p>
+                            ${data.view}回視聴
+                          </p>
+                        </div>
+                      </div>
+                    </div>`
+        ranking_result.append(html);
+      }
+      var category = $('#category_name').val()
+      $.ajax({
+        url: '/urls/research/',
+        type: 'GET',
+        data: {category: category},
+        dataType: 'json',
+        contentType: false
+      })
+      .done(function(data){
+        $(".card").remove();
+        data.forEach(function(url, index){
+          index += 1
+          rankingUrl(url, index);
+        });
+      })
+      .fail(function(){
+        alert('カテゴリーを選択してください')
+      })
+    })
 
     //url_form
   $('#first_url_name').change(function() {
